234abf351df997142298c07c5486feaddb858bc1
Rasel <aburaselahamed99@gmail.com>
2025-05-11 22:52:07 +0600

    start notes app assesment

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:notes_app_assesment/home_screen.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
