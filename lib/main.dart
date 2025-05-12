7f08a356ae4434b6a4071d2b72c4d37558283bb0
PTSL-RASEL <aburaselahamed99@gmail.com>
2025-05-12 15:23:22 +0600

    Firebase add use firebase cli

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:bloc_clean_architecture/config/routes/routes.dart';
 import 'package:bloc_clean_architecture/utils/theme/app_theme_data.dart';
-import 'package:bloc_clean_architecture/view/home/home_screen.dart';
-import 'package:bloc_clean_architecture/view/splash/splash_screen.dart';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import '';
-void main(){
-  runApp(MyApp());
+
+import 'firebase_options.dart';
+void main()async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
